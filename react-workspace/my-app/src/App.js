3b799725fb36fe2b93bf1da2aa13faae132be7a7
dabin <jdb4142@naver.com>
2023-12-08 10:40:41 +0900

    20231208_react

---
+++ react-workspace/my-app/src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+
+// import 여기서사용할이름 from ;'js파일경로';
+import ClassComponent from './components/R01_ClassComponent';
+import FunctionComponent from './components/R02_FunctionComponent';
+
+import Props1 from './components/R03_Props1';
+import Props2 from './components/R04_Props2';
+import Props3 from './components/R05_Props3';
+
+import State1 from './components/R06_State1';
+import State2 from './components/R07_State2';
+import State3 from './components/R08_State3';
+
+
+/* ===================================================== */
+/*
+  - React의 컴포넌트는 딱 하나의 요소만을 반환할 수 있다.
+  - 여러 요소를 반환하고 싶을 떄 부모 요소로 묶어준다!
+*/
+/* ===================================================== */
+function App() {
+  return (
+    /* <></> (fragment, 조각) : 반환되는 요소를 감싸는 태그, 해석 X */
+    <>
+      <h1>테스트</h1>
+      {/* <ClassComponent />
+
+      <hr />
+      <FunctionComponent />
+
+      <hr />
+      <Props1 />
+      <Props1 name='홍길동' num='1' />
+      <Props1 name='김철수' num='2' />
+
+      <hr />
+
+      <Props2 name="강감찬" age="72" address="낙성대" gender="남자" />
+      <Props2 name="천추태후" age="66" address="개경" gender="여자" />
+
+      <hr />
+      <Props3 name='김민규' />
+      <Props3 name='윤성국' /> */}
+
+      <hr />
+      <State1 />
+
+      <hr />
+      <State2 init={100} />
+      <State2 init={0} />
+
+      <hr />
+      <State3 />
+
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ react-workspace/my-app/src/components/R01_ClassComponent.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+
+
+// 클래스형 컴포넌트 만들기
+// 1. Component 상속 받기
+// 2. render() 함수 작성하기(필수)
+// 3. 만든 class를 export default 지정하기
+class ComponentEx extends Component {
+
+  // 생성자 함수 : 컴포넌트 생성 시 실행
+  constructor(props){
+    super(props);
+    
+    // 만들어지는 컴포넌트에 count:0이라는 상태 추가
+    // (java로 따지면 count 필드 만들고 0으로 초기화)
+    this.state = {count:0};    
+  }
+
+  // 이벤트 : 행동, 동작(click, ketup, mouseover...)
+  // 이벤트 리스너 : 이벤트 감지(onclick, onkeyup)
+  // 이벤트 핸들러 : 이벤트 감지 시 수행할 함수(기능)
+
+  /* 이벤트 핸들러 작성 */
+  clickHandler = () => {
+    this.setState({count : this.state.count+ 1 });
+  }
+
+
+  // render() 함수 :
+  // return 되는 html형식의 코드(jsx, js + xml)를 화면에 출력하는 함수
+  // 화면이 변경되어야 할 시점에 자동으로 호출됨.
+  // ** Component를 상속 받은 하위 클래스에 반드시 있어야 하는 함수**
+  render() { 
+    return(
+      <>
+        <h2>클래스형 컴포넌트 입니다.</h2>
+        
+        <h1>count : {this.state.count}</h1>
+        <button onClick={this.clickHandler}>증가</button>
+      </>
+    );
+  }
+}
+
+export default ComponentEx;
